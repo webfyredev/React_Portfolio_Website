6b2c384e775b3036dd2fb2b1e973805105cf095c
impphilwebs <imphilwebs@gmail.com>
2025-12-24 00:38:23 +0100

    Added a title to the page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,8 +7,12 @@ import Project from "../components/projects";
 import Faqs from "../components/faq";
 import Contacts from "../components/contacts";
 import { TechnicalSkills } from "../components/skill";
+import { useEffect } from "react";
 
 export default function Home_Page(){
+    useEffect(() => {
+        document.title = 'Oyinloye Oyinlade - Fullstack Website Developer'
+    });
     return(
         <>
             <NavBar />
